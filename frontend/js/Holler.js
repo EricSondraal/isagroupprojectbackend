d4892b5179b757abf3fa15db54926dd33c0005c6
RishamJo <johar.risham@gmail.com>
2021-04-10 21:24:57 -0700

    Added some posting functions

---
+++ frontend/js/Holler.js
@@ -0,0 +1,61 @@
+function Holler(postNum, user, title, body, isPersonal) {
+    this.postNum = postNum;
+    this.user = user;
+    this.title = title;
+    this.body = body;
+    this.isPersonal = isPersonal;
+
+    this.containerDiv;
+    this.display = () => {
+        this.containerDiv = document.createElement("div");
+        this.containerDiv.setAttribute("class", "card");
+
+        let bodyDiv = document.createElement("div");
+        bodyDiv.setAttribute("class", "card-body");
+
+        let cardTitle = document.createElement("h4");
+        cardTitle.setAttribute("class", "card-title");
+        cardTitle.innerHTML = this.title;
+
+        let cardUser = document.createElement("h6");
+        cardUser.setAttribute("class", "card-subtitle mb-2 text-muted");
+        cardUser.innerHTML = this.user;
+
+        let cardBody = document.createElement("p");
+        cardBody.setAttribute("class", "card-text");
+        cardBody.innerHTML = this.body;
+
+        bodyDiv.appendChild(cardTitle);
+        bodyDiv.appendChild(cardUser);
+        bodyDiv.appendChild(cardBody);
+        this.containerDiv.appendChild(bodyDiv);
+
+        if (isPersonal) {
+            let deleteButton = document.createElement("button");
+            deleteButton.setAttribute("class", "card-link");
+            deleteButton.innerHTML = "Delete";
+            deleteButton.onclick = this.delete;
+
+            bodyDiv.appendChild(deleteButton);
+        }
+
+        return this.containerDiv;
+    }
+
+    this.delete = () => {
+        let postObject = {
+            name: this.user,
+            postNumber: this.postNum
+        };
+        let xmlDelete = new XMLHttpRequest();
+        xmlDelete.open("DELETE", `${apiURL}deletecomment`);
+        xmlDelete.setRequestHeader('Content-Type', 'application/json');
+        xmlDelete.setRequestHeader('x-api-key', apiKey);
+        xmlDelete.onreadystatechange = () => {
+            if (xmlDelete.readyState === XMLHttpRequest.DONE && xmlDelete.status === 200){
+                location.reload();
+            }
+        }
+        xmlDelete.send(JSON.stringify(accountInfo));
+    }
+}
---
+++ frontend/js/myholler.js
@@ -0,0 +1,89 @@
+let hollers = [];
+
+window.addEventListener('DOMContentLoaded', () => {
+    if (username) {
+        //Create holler list and form for new ones.
+        document.getElementById("hollerSpace").innerHTML =
+            `<div id="addForm" class="float-right mx-5 mt-5">
+            <h4>Send a new Holler!</h4>
+            <form id="newHollerForm">
+                <input type="text" name="hollertitle" placeholder="Holler Title">
+                <br>
+                <textarea rows="3" cols="50" name="hollertext"></textarea>
+                <br>
+                <input type="submit" value="Holler!">
+            </form>
+        </div>
+        <ul id="myHollers">
+            <li>Loading Hollers...</li>
+        </ul>`;
+        document.getElementById("newHollerForm").onsubmit = submitHoller;
+        getHollers();
+        showHollers();
+    }
+});
+
+function submitHoller(event) {
+    event.preventDefault();
+    let data = new FormData(event.target);
+    let hollerData = {
+        name: username,
+        title: data.get("hollertitle"),
+        body: data.get("hollertext")
+    };
+    console.log(hollerData);
+    let xmlHoller = new XMLHttpRequest();
+    xmlHoller.open("POST", `${apiURL}newcomment`);
+    xmlHoller.setRequestHeader('Content-Type', 'application/json');
+    xmlHoller.setRequestHeader('x-api-key', apiKey);
+    xmlHoller.onreadystatechange = () => {
+        if (xmlHoller.readyState === XMLHttpRequest.DONE && xmlHoller.status === 200) {
+            let response = JSON.parse(xmlHoller.responseText);
+            if (response.result === "success") {
+                console.log(response);
+                getHollers();
+            }
+        }
+    }
+    xmlHoller.send(JSON.stringify(hollerData));
+}
+
+function getHollers() {
+    let fetchHoller = new Promise(resolve => {
+        let xmlCount = new XMLHttpRequest();
+        xmlCount.open("GET", `${apiURL}postcount/${username}`);
+        xmlCount.setRequestHeader("x-api-key", apiKey);
+        xmlCount.onreadystatechange = () => {
+            if (xmlCount.readyState === XMLHttpRequest.DONE && xmlCount.status === 200) {
+                let response = JSON.parse(xmlCount.responseText);
+                resolve(response.postcount);
+            }
+        }
+        xmlCount.send();
+    }).then(result => {
+        let myHollers = document.getElementById("myHollers");
+        if (result > 0) {
+            myHollers.innerHTML = "";
+            for (let i = 0; i < result; i++) {
+                let xmlGetHoller = new XMLHttpRequest();
+                xmlGetHoller.open("GET", `${apiURL}getcomment/${username}/${i}`);
+                xmlGetHoller.setRequestHeader('x-api-key', apiKey);
+                xmlGetHoller.onreadystatechange = () => {
+                    if (xmlGetHoller.readyState === XMLHttpRequest.DONE && xmlGetHoller.status === 200){
+                        let response = JSON.parse(xmlGetHoller.responseText);
+                        hollers.push(new Holler(i, username, response.title, response.body, true));
+                    }
+                }
+                xmlGetHoller.send();
+            }
+        } else {
+            myHollers.innerHTML = "No Hollers";
+        }
+    });
+}
+
+function showHollers(){
+    hollers.forEach(element => {
+        document.getElementById("hollerSpace").appendChild(element.display());
+    });
+}
